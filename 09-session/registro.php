e002ac1dea71960c25cdcf63d458fbdb403c643d
ImOch8a <136657859+ImOch8a@users.noreply.github.com>
2024-05-30 16:04:48 -0500

    Sesion con cambios

--- 09-session/registro.php
+++ 09-session/registro.php
@@ -3,7 +3,9 @@
  if ($_SERVER['REQUEST_METHOD'] == 'POST'){  
     $usuario = $_POST ['username'];
     $password = $_POST ['password'];
+    $password_2 = $_POST ['password_2'];
     $email = $_POST ['email'];
+    
 }
 
 if(empty($usuario) or empty($password)){
@@ -12,6 +14,7 @@ if(empty($usuario) or empty($password)){
 
     $_SESSION['userRegister'] = $usuario;
     $_SESSION['passRegister'] = $password;
+    $_SESSION['pass2Register'] = $password_2;
     $_SESSION['emailRegister'] = $email;
     
     try {
@@ -26,9 +29,13 @@ if(empty($usuario) or empty($password)){
     $statement->execute(array(":username"=>$usuario,":email"=>$email,":pass"=>$password));
 
     $statement = $statement->fetchAll();
-}
 
-    echo 'datos enviados';
+    if($password == $password_2){
+        echo "datos enviados";
+    }else{
+        echo "Las contraseñas no coinciden";
+    }
+}
 
 ?>
 
@@ -53,15 +60,22 @@ if(empty($usuario) or empty($password)){
     <label for="email">Email</label>
     <input type="email" id="email" placeholder="Email" name="email" required> <br>
     <label for="password">Password</label>
-    <input id="password" type="password" placeholder="Password" name="password">
+    <input id="password" type="password" placeholder="Password" name="password"> <br>
+    <label for="password_2">Password</label>
+    <input id="password_2" type="password" placeholder="Confirme la contraseña" name="password_2"> <br>
     <button type="submit">Registro</button>
 
     </form>
 
-    <?php if( isset($_SESSION['userRegister'])): ?>
-    <p>Datos registraos, ya puedes iniciar sesion</p>
-    <p><?php echo $_SESSION['userRegister'] . '<br>' . $_SESSION['passRegister'] . '<br>' . $_SESSION['emailRegister']; ?></p>
-    <a href="index.php">Iniciar Sesion</a>
-    <?php endif ?>    
+    <?php if ($_SESSION['passRegister'] == $_SESSION['pass2Register']){
+        echo "Datos registaos  <br> $usuario <br> $password <br> $email";
+    }else{
+        echo "se cancela";
+    }
+
+?>
+<br>
+<a href="index.php">Iniciar Sesion</a>
+
 </body>
 </html>
